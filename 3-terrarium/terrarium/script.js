04dc661f86bfae15a897d5c1eb0196ff0780d4d0
Nestoralons <nestoralons42@gmail.com>
2022-08-12 17:31:56 -0400

    Terrario listo

---
+++ 3-terrarium/terrarium/script.js
@@ -0,0 +1,56 @@
+
+dragElement(document.getElementById('plant1'));
+dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant4'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
+dragElement(document.getElementById('plant7'));
+dragElement(document.getElementById('plant8'));
+dragElement(document.getElementById('plant9'));
+dragElement(document.getElementById('plant10'));
+dragElement(document.getElementById('plant11'));
+dragElement(document.getElementById('plant12'));
+dragElement(document.getElementById('plant13'));
+dragElement(document.getElementById('plant14'));
+
+// Creamos una funcion
+function dragElement(elemento) {
+    let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0,
+	pos5=0,
+	pos6=0;
+elemento.onpointerdown = pointerDrag;
+function pointerDrag(e) {
+	e.preventDefault();
+	console.log(e);
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	pos5 = e.clientX;
+	pos6 = e.clientY;
+    document.onpointermove = elementDrag;
+document.onpointerup = stopElementDrag;
+document.ondblclick= dobleclick;
+} 
+function elementDrag(e) {
+	pos1 = pos3 - e.clientX;
+	pos2 = pos4 - e.clientY;
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	console.log(pos1, pos2, pos3, pos4);
+	elemento.style.top = elemento.offsetTop - pos2 + 'px';
+	elemento.style.left = elemento.offsetLeft - pos1 + 'px';
+}
+function stopElementDrag() {
+	document.onpointerup = null;
+	document.onpointermove = null;
+}
+function dobleclick(e){
+	e.preventDefault();
+	elemento.style.top = 0 + 'px';
+	elemento.style.left = 0 + 'px';
+	
+}
+}
